1b6781a068627c3f81b631e15feb0f4eeb8b4262
Adam Koulopoulos <adam@Adams-MacBook-Pro.local>
2023-11-21 17:43:17 -0500

    .

--- lab5.js
+++ lab5.js
@@ -7,22 +7,103 @@ const assignment = {
   score: 0,
 };
 
-function Lab5(app) {
+const todos = [
+  { id: 1, title: "Task 1", completed: true },
+  { id: 2, title: "Task 2", completed: false },
+  { id: 3, title: "Task 3", completed: true },
+  { id: 4, title: "Task 4", completed: false },
+];
+
+const Lab5 = (app) => {
+  app.get("/a5/todos/:id/title/:title", (req, res) => {
+    const { id, title } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = title;
+    res.json(todos);
+  });
+
+  app.get("/a5/todos/:id/delete", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todos.splice(todos.indexOf(todo), 1);
+    res.json(todos);
+  });
+
+  app.put("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    todo.due = req.body.due;
+    todo.completed = req.body.completed;
+    res.sendStatus(200);
+  });
+
+  app.delete("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.sendStatus(404);
+      return;
+    }
+
+    todos.splice(todos.indexOf(todo), 1);
+    // res.json(todos);
+    res.sendStatus(204);
+  });
+
+  app.post("/a5/todos", (req, res) => {
+    const newTodo = {
+      ...req.body,
+      id: new Date().getTime(),
+    };
+    todos.push(newTodo);
+    res.json(newTodo);
+  });
+
+  app.get("/a5/todos/create", (req, res) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      title: "New Task",
+      completed: false,
+    };
+    todos.push(newTodo);
+    res.json(todos);
+  });
+  app.get("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((todo) => todo.id === parseInt(id));
+    res.json(todo);
+  });
+
+  // http://localhost:3000/a5/todos?completed=true
+  app.get("/a5/todos", (req, res) => {
+    const { completed, due, assignedTo } = req.query;
+    if (completed === "true") {
+      const completedTodos = todos.filter((todo) => todo.completed);
+      res.json(completedTodos);
+      return;
+    } else if (completed === "false") {
+      const incompletedTodos = todos.filter((todo) => !todo.completed);
+      res.json(incompletedTodos);
+      return;
+    }
+    res.json(todos);
+  });
+
   app.get("/a5/assignment", (req, res) => {
     res.json(assignment);
+  });
+  app.get("/a5/assignment/title", (req, res) => {
+    res.json(assignment.title);
   });
   app.get("/a5/assignment/title/:newTitle", (req, res) => {
     const { newTitle } = req.params;
     assignment.title = newTitle;
     res.json(assignment);
   });
-  app.get("/a5/assignment/title", (req, res) => {
-    res.json(assignment.title);
-  });
-  app.get("/a5/assignment/description", (req, res) => {
-    res.json(assignment.description);
-  });
-  app.get("/a5/calculator?a=:a&b=:b", (req, res) => {
+
+  app.get("/a5/calculator", (req, res) => {
     const { a, b, operation } = req.query;
     let result = 0;
     switch (operation) {
@@ -50,8 +131,7 @@ function Lab5(app) {
   });
 
   app.get("/a5/welcome", (req, res) => {
-    res.send("Welcome to Lab 5!");
+    res.send("Welcome to Assignment 5 !!!!!!");
   });
-}
-
+};
 export default Lab5;
